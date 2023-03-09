e9d528d5256ecb4da0cd75d25d8710748515b86a
M.Hassan Raza <mhassan.work0@gmail.com>
2023-03-09 15:30:01 +0500

    Initial Commit

---
+++ sticker/stickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickerApp.swift
+//  sticker
+//
+//  Created by Hassan Raza on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct stickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
